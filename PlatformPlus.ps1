9a1a6bf19f439322b07bc45f2bda23f283ed7fd4
dnlrv <kaisenai@gmail.com>
2022-01-18 16:31:19 -0500

    Added Get-PlatformRowAce with Secret support

--- PlatformPlus.ps1
+++ PlatformPlus.ps1
@@ -164,6 +164,108 @@ function global:Get-PlatformObjectUuid
 ###########
 
 ###########
+#region ### global:Convert-PermissionToString # Converts a Grant integer permissions number to readable permissions
+###########
+function global:Convert-PermissionToString
+{
+    param
+    (
+        [Parameter(Mandatory = $true, HelpMessage = "The type of permission to convert.")]
+        [System.String]$Type,
+
+        [Parameter(Mandatory = $true, HelpMessage = "The Grant (int) number of the permissions to convert.")]
+        [System.Int32]$PermissionInt
+    )
+
+    # setting our return value
+    [System.String]$ReturnValue = ""
+
+    # setting our readable permission hash based on our object type
+    switch ($Type)
+    {
+        "Secret" { $AceHash = @{ GrantSecret = 1; ViewSecret = 4; EditSecret = 8; DeleteSecret= 64; RetrieveSecret = 65536} ; break }
+    }
+
+    # for each bit (sorted) in our specified permission hash
+    foreach ($bit in ($AceHash.GetEnumerator() | Sort-Object))
+    {
+        # if the bit seems to exist
+        if (($PermissionInt -band $bit.Value) -ne 0)
+        {
+            # add the key to our return string
+            $ReturnValue += $bit.Key + "|"
+        }
+    }
+
+    # return the string, removing the trailing "|"
+    return ($ReturnValue.TrimEnd("|"))
+}
+#}# global:Convert-PermissionToString
+#endregion
+###########
+
+###########
+#region ### global:Get-PlatformRowAce # Gets RowAces for the specified platform object
+###########
+function global:Get-PlatformRowAce
+{
+    param
+    (
+        [Parameter(Mandatory = $true, HelpMessage = "The type of object to search.")]
+        [System.String]$Type,
+
+        [Parameter(Mandatory = $true, HelpMessage = "The name of the object to search.")]
+        [System.String]$Name
+    )
+
+    # getting the uuid of the object
+    $uuid = Get-PlatformObjectUuid -Type $Type -Name $Name
+
+    # setting the table variable
+    [System.String]$table = ""
+
+    Switch ($Type)
+    {
+        "Secret" { $table = "DataVault" ; break }
+    }
+
+    # preparing the JSONBody
+    $JSONBody = @{ RowKey = $uuid ; Table = $table } | ConvertTo-Json
+
+    # getting the RowAce information
+    $RowAces = Invoke-PlatformAPI -APICall Acl/GetRowAces -Body $JSONBody
+
+    # setting a new ArrayList for the return
+    $RowAceObjects = New-Object System.Collections.ArrayList
+
+    # for each rowace retrieved
+    foreach ($rowace in $RowAces)
+    {
+        # ignore any global root entries
+        if ($rowace.Type -eq "GlobalRoot")
+        {
+            continue
+        }
+
+        # creating the PlatformPermission object
+        $platformpermission = [PlatformPermission]::new($Type, $rowace.Grant, `
+                              $rowace.GrantStr)
+
+        # creating the PlatformRowAce object
+        $obj = [PlatformRowAce]::new($rowace.PrincipalType, $rowace.Principal, `
+               $rowace.PrincipalName, $rowace.AceID, $platformpermission)
+
+        # adding the PlatformRowAce object to our ArrayList
+        $RowAceObjects.Add($obj) | Out-Null
+    }# foreach ($rowace in $RowAces)
+
+    # returning the RowAceObjects
+    return $RowAceObjects
+}# function global:Get-PlatformRowAce
+#endregion
+###########
+
+###########
 #region ### global:TEMPLATE # TEMPLATE
 ###########
 #function global:Invoke-TEMPLATE
@@ -171,6 +273,52 @@ function global:Get-PlatformObjectUuid
 #}# function global:Invoke-TEMPLATE
 #endregion
 ###########
+
+#######################################
+#endregion ############################
+#######################################
+
+#######################################
+#region ### CLASSES ###################
+#######################################
+
+# class for holding Permission information including converting it to
+# a human readable format
+class PlatformPermission
+{
+    [System.String]$Type        # the type of permission (Secret, Account, etc.)
+    [System.Int64]$GrantInt     # the Int-based number for the permission mask
+    [System.String]$GrantBinary # the binary string of the the permission mask
+    [System.String]$GrantString # the human readable permission mask
+
+    PlatformPermission ([System.String]$t, [System.Int64]$gi, [System.String]$gb)
+    {
+        $this.Type        = $t
+        $this.GrantInt    = $gi
+        $this.GrantBinary = $gb
+        $this.GrantString = Convert-PermissionToString -Type $t -PermissionInt $gi
+    }
+}# class PlatformPermission
+
+# class for holding RowAce information
+class PlatformRowAce
+{
+    [System.String]$PrincipalType           # the principal type
+    [System.String]$PrincipalUuid           # the uuid of the prinicpal
+    [System.String]$PrincipalName           # the name of the principal
+    [System.String]$AceID                   # the uuid of this RowAce
+    [PlatformPermission]$PlatformPermission # the platformpermission object
+
+    PlatformRowAce([System.String]$pt, [System.String]$puuid, [System.String]$pn, `
+                   [System.String]$aid, [PlatformPermission]$pp)
+    {
+        $this.PrincipalType      = $pt
+        $this.PrincipalUuid      = $puuid
+        $this.PrincipalName      = $pn
+        $this.AceId              = $aid
+        $this.PlatformPermission = $pp
+    }
+}# PlatformRowAce
 
 #######################################
 #endregion ############################
